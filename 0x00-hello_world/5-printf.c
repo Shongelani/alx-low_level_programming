ba4095b0f9d581fbc9b01b3422730b2b1c961473
Shongelani <Shongelani01@gmail.com>
2023-06-15 12:55:49 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - prints a line with a printf function
+ *Return : Always  0 (Success)
+ * */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n ");
+	return (0);
+}

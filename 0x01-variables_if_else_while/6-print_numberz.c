db27c94ed138aea2eb5c859c1ada56b8bceb27ce
Shongelani <Shongelani01@gmail.com>
2023-06-18 13:02:01 -0700

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + 48);
+	}
+	putchar('\n');
+	return (0);
+}
